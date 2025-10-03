0ffe637d4f8dd8f898906acaa1dac9b26781350f
Ravi Sahani <ravisahani2893@gmail.com>
2025-10-03 22:25:37 +0100

    "Fetched all 608 Nasa space biology data"

--- space-enginer.py
+++ space-enginer.py
@@ -46,7 +46,7 @@ def fetch_space_biology_data_bioc(pmcid: str):
 
     
     url = f"https://www.ncbi.nlm.nih.gov/research/bionlp/RESTful/pmcoa.cgi/BioC_json/{pmcid}/unicode"
-    print(pmcid)
+    # print(pmcid)
     response = requests.get(url)
 
     if response.status_code != 200:
@@ -55,7 +55,7 @@ def fetch_space_biology_data_bioc(pmcid: str):
 
     try:
         paper_json = response.json()
-        print(paper_json)
+        # print(paper_json)
     except Exception as e:
         print(f"Failed to parse JSON for {pmcid}: {e}")
         return None
@@ -84,23 +84,64 @@ def get_all_paper_data(df):
 
 table_metadata_ids = get_all_paper_data(df)
 
+def process_space_biology_data(metada_json_data):
+    paragraph_data = metada_json_data[0]["documents"][0]
+    passsages =paragraph_data['passages']
 
 
-def fetch_all_nasa_metada_info(metadata_list):
+    rows = []
+    current_l1 = None
+    current_l2 = None
+
+    for item in passsages:
+        passagge_data = item["text"]
+        t = item['infons']['type']
+        text = cleanup_data(passagge_data)
+
+        if t == "title_1":
+            current_l1 = text
+            current_l2 = None
+        elif t == "title_2":
+            current_l2 = text
+        elif t == "paragraph":
+            rows.append({
+            "level_1": current_l1,
+            "level_2": current_l2,
+            "text": text
+            })
+
+
+    return rows
+
+def fetch_all_nasa_metadata_info(metadata_list):
 
     metadata_raw_json_response_list=list()
 
-    # raw_json_data=fetch_space_biology_data_bioc(metadata_list[1])
-
-    for metadata_id in metadata_list:
-        raw_json_data=fetch_space_biology_data_bioc(metadata_id)
-        metadata_raw_json_response_list.append(raw_json_data)
+    raw_json_data=fetch_space_biology_data_bioc(metadata_list[1])
+    space_engine_processed_data = process_space_biology_data(raw_json_data)
+    row_json = json.dumps(space_engine_processed_data)
 
 
-    return metadata_raw_json_response_list  
+    print(row_json)
+
+    # for metadata_id in metadata_list:
+    #     raw_json_data=fetch_space_biology_data_bioc(metadata_id)
+        
+    #     space_engine_processed_data = process_space_biology_data(raw_json_data)
+
+    #     try:
+    #         raw_json_data = json.loads(space_engine_processed_data)
+    #         print("Valid JSON")
+    #     except json.JSONDecodeError:
+    #         print("Invalid JSON")
+
+    #     metadata_raw_json_response_list.append(raw_json_data)
 
 
-metadata_raw_json_response_list=fetch_all_nasa_metada_info(table_metadata_ids)
+    # return metadata_raw_json_response_list  
+
+
+metadata_raw_json_response_list=fetch_all_nasa_metadata_info(table_metadata_ids)
 
 
 # print(metadata_raw_json_response_list[0])
